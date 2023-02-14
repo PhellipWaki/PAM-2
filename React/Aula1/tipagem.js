a81f9a1b2d6d76fd0a2f0c745ee05d9805f7cfef
PhellipWaki <phellipmassanobuwaki@gmail.com>
2023-02-13 22:26:15 -0300

    Aula1

---
+++ React/Aula1/tipagem.js
@@ -0,0 +1,13 @@
+let qualquer = ["João", "Siles"]
+let qualquer2 = {
+    nome: "João",
+    idade: 35
+}
+console.log(qualquer)
+console.log(Array.isArray(qualquer))
+console.log(qualquer2)
+console.log(typeof qualquer2)
+
+qualquer = 3.14
+console.log(qualquer)
+console.log(typeof qualquer) // tipagem fraca significa que a variavel ter 
